079db0aea51b33be25ac1cd726f33673629a3e13
Kaayaeeshan <kaayesjr113@gmail.com>
2025-11-04 00:11:01 +0530

    feat: Added admin_header.php

---
+++ components/admin_header.php
@@ -0,0 +1,16 @@
+<!-- header section starts -->
+
+<header class="header">
+
+    <section class="flex">
+        <a href="dashboard.php" class="logo">Admin.</a>
+
+        <form action="search_page.php" method="post" class="search-form">
+        </form>
+
+    </section>
+
+</header>
+
+
+<!-- header section ends -->
