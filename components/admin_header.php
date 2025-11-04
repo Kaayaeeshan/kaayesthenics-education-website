1ba48a828af1a3a9832da5e0b69bf8828ac213e2
Kaayaeeshan <kaayesjr113@gmail.com>
2025-11-04 16:13:00 +0530

    added button and check

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,12 +1,18 @@
 <!-- header section starts -->
-
+ 
 <header class="header">
 
     <section class="flex">
         <a href="dashboard.php" class="logo">Admin.</a>
 
         <form action="search_page.php" method="post" class="search-form">
+            <input type="text" placeholder= "search here...." required maxlength="100" name= "search_box"/>
+            <button type= "submit" class="fas fa-search" name= "search" ></button>
         </form>
+
+        <div class="icons">
+
+        </div>
 
     </section>
 
