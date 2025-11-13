00c66d6ff92f1367a852134b2d43ccf98ab06f82
Kaayaeeshan <kaayesjr113@gmail.com>
2025-11-13 12:53:31 +0530

    feat: adding files

---
+++ admin/dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+<!-- font awesome cdn link -->
+ <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+<!-- custom CSS file link -->
+ <link rel = "stylesheet" href = "../css/admin_style.css">
+
+</head>
+<body>
+    
+<!-- header section link -->
+<?php include "../components/admin_header.php"; ?>
+
+
+
+
+
+
+
+<!-- footer section link -->
+ <?php include "../components/footer.php"; ?>
+
+<!-- custom Javascript file link -->
+<script src = "../js/admin_script.js" ></script>
+
+</body>
+</html>
